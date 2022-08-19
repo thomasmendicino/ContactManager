17a4bfc4c240e0d589007df3c8b3c6f2b830ec04
Thomas Mendicino <tmendicino@gmail.com>
2022-08-19 16:59:54 -0700

    In-progress model creation

---
+++ ContactManager.Tests/Models/ContactTests.cs
@@ -0,0 +1,15 @@
+﻿//using FluentAssertions;
+
+namespace ContactManager.Tests.Models
+{
+    public class ContactTests
+    {
+        [Fact]
+        public void NewContacts_Should_SetBaseProperties()
+        {
+            //Contact contact = new Contact();
+
+            //contact.Should().Have
+        }
+    }
+}
---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContactManager.Models
+{
+    internal abstract class Contact
+    {
+        private string _name;
+        private string _address;
+        private string _phone;
+        private string _company;
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return this._address;
+            }
+            set
+            {
+                this._address = value;
+            }
+        }
+
+        public string Phone { get; set; }
+    }
+}
