4059b9244ede09cb287e85e8597fecef4fe8e935
Thomas Mendicino <tmendicino@gmail.com>
2022-08-23 16:39:57 -0700

    Remove unnecessary company model

--- ContactManager/Models/Vendor.cs
+++ ContactManager/Models/Vendor.cs
@@ -8,5 +8,12 @@ namespace ContactManager.Models
 {
     public class Vendor : Contact
     {
+        private string? _vendorCode;
+
+        public string? VendorCode
+        {
+            get { return _vendorCode; }
+            set { _vendorCode = value; }
+        }
     }
 }
