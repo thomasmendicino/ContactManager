e8ecbbda7440864f72c4615af6a3bf1180ccd361
Thomas Mendicino <tmendicino@gmail.com>
2022-08-29 14:44:44 -0700

    Remove unused libraries

--- ContactManager/Stores/NavigationStore.cs
+++ ContactManager/Stores/NavigationStore.cs
@@ -1,9 +1,5 @@
 ﻿using ContactManager.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ContactManager.Stores
 {
